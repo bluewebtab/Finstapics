72df89fe1bd5262305a4cf68469da25edf9b2b09
Ronny Gutierrez <ronny.icne@gmail.com>
2020-05-07 22:37:28 -0700

    Updates

---
+++ precache-manifest.0c27b6859a67eca3484e5d62c2083d00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b245b5b71ae5a2e9c4b1a4961c20224",
+    "url": "/Finstapics/index.html"
+  },
+  {
+    "revision": "64a3f53ce86c07f9beb3",
+    "url": "/Finstapics/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "249be69d0b898c11c089",
+    "url": "/Finstapics/static/css/main.beee0264.chunk.css"
+  },
+  {
+    "revision": "64a3f53ce86c07f9beb3",
+    "url": "/Finstapics/static/js/2.7a400a68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Finstapics/static/js/2.7a400a68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "249be69d0b898c11c089",
+    "url": "/Finstapics/static/js/main.9f3f7ea8.chunk.js"
+  },
+  {
+    "revision": "d15af2d30815025f67c3",
+    "url": "/Finstapics/static/js/runtime-main.68ac9c7c.js"
+  }
+]);
