080cba45212dbb0a59e91a7bd49eecd0d2fefc15
Ronny Gutierrez <ronny.icne@gmail.com>
2020-05-14 15:05:00 -0700

    changed package.json

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const path = require('path');
 const app = express();
 
 
 
-app.listen((process.env.PORT || 3000), function(){
-    console.log('listening on *:3000');
-  });
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
