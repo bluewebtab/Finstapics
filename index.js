2837edab66b8686e4d826b31ac08ce33ffe96eba
Ronny Gutierrez <ronny.icne@gmail.com>
2020-05-14 16:16:02 -0700

    package.json: added engine versions

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 3000;
+// const express = require('express')
+// const path = require('path');
+// const app = express();
+// const port = process.env.PORT || 3000;
 
 
-server.listen(port, function() {
-    console.log("App is running on port " + port);
-});
+// server.listen(port, function() {
+//     console.log("App is running on port " + port);
+// });
 
-app.get('/', function(req, res){
-    res.redirect('/finstapics')
-}
+// app.get('/', function(req, res){
+//     res.redirect('/finstapics')
+// }
 
 // app.use(express.static(path.join(__dirname, 'build')));
 
