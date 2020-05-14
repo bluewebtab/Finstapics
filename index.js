ffe11f14d74a35313361bda76c96e5d1b5d6cb26
Ronny Gutierrez <ronny.icne@gmail.com>
2020-05-14 14:09:23 -0700

    changed index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 3000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+express().listen((process.env.PORT || 3000), function(){
+    console.log('listening on *:3000');
+  });
