6c889d74c42a259703d122fc10d3f790f3c1fcda
Ronny Gutierrez <ronny.icne@gmail.com>
2020-05-14 16:04:11 -0700

    changed index.js

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ server.listen(port, function() {
     console.log("App is running on port " + port);
 });
 
+app.get('/', function(req, res){
+    res.redirect('/finstapics')
+}
+
+// app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
