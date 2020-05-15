2629027619ce8a490f72fde09221f02d5397ec5c
Ronny Gutierrez <ronny.icne@gmail.com>
2020-05-14 19:40:40 -0700

    Updates

---
+++ precache-manifest.d3296bb90cda100895a46b00296fb776.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036a7b09f323efd2d3657af576112385",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce41f55d9fedd211056f",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "1b9832bd8cdbc32223e1",
+    "url": "/static/css/main.18d2c79e.chunk.css"
+  },
+  {
+    "revision": "ce41f55d9fedd211056f",
+    "url": "/static/js/2.c994a98d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c994a98d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b9832bd8cdbc32223e1",
+    "url": "/static/js/main.fd8666fb.chunk.js"
+  },
+  {
+    "revision": "e6e4e9999da9c8bc9214",
+    "url": "/static/js/runtime-main.9abd5c09.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Finstapics/precache-manifest.0c27b6859a67eca3484e5d62c2083d00.js"
+  "/precache-manifest.d3296bb90cda100895a46b00296fb776.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Finstapics/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
